e33c539605c0b0b26f2d7de1748aed8122270538
nullus <dylan.perry@gmail.com>
2018-01-21 16:09:13 +1100

    Update fish config to work on FreeBSD host

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,38 @@
 # Build using homebrew OpenSSL
-set -x LDFLAGS -L(brew --prefix openssl)"/lib"
-set -x CFLAGS -I(brew --prefix openssl)"/include"
+if type -q brew
+    set -x LDFLAGS -L(brew --prefix openssl)"/lib"
+    set -x CFLAGS -I(brew --prefix openssl)"/include"
+end
 
 if status --is-interactive
     # Alias for dotfiles git config
-    alias dotfile="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+    alias dotfile="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
     # Configure rbenv
-    source (rbenv init -|psub)
+    if type -q rbenv
+        source (rbenv init -|psub)
+    end
+
+    if test -e ~/.pyenv
+        # Configure for local .pyenv
+        set -x PYENV_ROOT ~/.pyenv
+        set PATH $PYENV_ROOT/bin $PATH
+    end
 
     # Configure pyenv
-    source (pyenv init -|psub)
-    source (pyenv virtualenv-init -|psub)
+    if type -q pyenv
+        source (pyenv init -|psub)
+        source (pyenv virtualenv-init -|psub)
+    end
 
     # Add SSH keys
     if not ssh-add -l >/dev/null
-        ssh-add -K -A
+        set _ssh_add_args ""
+        if test (uname -s) = "Darwin"
+            set _ssh_add_args $_ssh_add_args -K
+        end
+        ssh-add $_ssh_add_args -A
     end
+
+    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 end

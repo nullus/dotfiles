99af6661d994b9db82f161660d1036bb3dec5b63
Dylan Perry <dylan.perry@messagemedia.com.au>
2018-01-23 15:23:44 +1100

    Provide STS token expiration information, tweak AWS configuration

--- .config/fish/functions/aws_update_sts.fish
+++ .config/fish/functions/aws_update_sts.fish
@@ -2,13 +2,17 @@ function aws_update_sts --description 'Create session token using MFA'
     # Requires a basic AWS configuration containing:
     # - mfa_serial in [default] section;
     # - [default] credentials (authentication account)
-	set TMPFILE (mktemp)
+    if not _aws_sts_expired
+        # Don't regenerate token if it's still valid
+        return 0
+    end
+    set TMPFILE (mktemp)
     or begin
         echo "Failed to create temporary credential file" >&2
         return 1
     end
     read -P 'Enter MFA code: ' token_code
-    aws sts get-session-token --serial-number (aws configure get default.mfa_serial) --token-code "$token_code" > $TMPFILE
+    aws sts --output json --profile default get-session-token --serial-number (aws configure get default.mfa_serial) --token-code "$token_code" > $TMPFILE
     or begin
         echo "Failed to create sts session token" >&2
         return 1
@@ -16,6 +20,7 @@ function aws_update_sts --description 'Create session token using MFA'
     aws configure set profile.token.aws_access_key_id (jq -r .Credentials.AccessKeyId $TMPFILE)
     aws configure set profile.token.aws_secret_access_key (jq -r .Credentials.SecretAccessKey $TMPFILE)
     aws configure set profile.token.aws_session_token (jq -r .Credentials.SessionToken $TMPFILE)
+    set -Ux __aws_sts_expiration (jq -r '.Credentials.Expiration|fromdateiso8601' $TMPFILE) 
     rm $TMPFILE
     or begin
        echo "Failed to remove temporary credential file: $TMPFILE" >&2

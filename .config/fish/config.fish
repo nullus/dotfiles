808c23a0f37ac714c2c8485c4086a9c8eb398f94
Dylan Perry <dylan.perry@messagemedia.com.au>
2018-11-13 17:49:44 +1100

    Profile: fix LDFLAGS/CPPFLAGS to against brew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 # Build using homebrew OpenSSL
 if type -q brew
-    set -gx LDFLAGS -L(brew --prefix openssl)"/lib" "-L/usr/local/opt/zlib/lib" "-L/usr/local/opt/sqlite/lib"
-    set -gx CPPFLAGS -I(brew --prefix openssl)"/include" "-I/usr/local/opt/zlib/include" "-I/usr/local/opt/sqlite/include"
-    set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig" "/usr/local/opt/sqlite/lib/pkgconfig"
+    set -gx LDFLAGS "-L"(brew --prefix openssl)"/lib -L/usr/local/opt/zlib/lib -L/usr/local/opt/sqlite/lib"
+    set -gx CPPFLAGS "-I"(brew --prefix openssl)"/include -I/usr/local/opt/zlib/include -I/usr/local/opt/sqlite/include"
+    set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig:/usr/local/opt/sqlite/lib/pkgconfig"
     # Prefer OpenSSL installed by homebrew
     set PATH (brew --prefix openssl)"/bin" $PATH
 end
